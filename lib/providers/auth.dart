ab10d2856c0e4379b216d79c794590ac370c4cf0
muza <muzallyefah@petotumcharities.org>
2023-10-24 18:37:24 +0800

    feat: register page

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
 class AuthProvider with ChangeNotifier {
-  bool _obscureLoginPass = true;
-  bool _isLoginProcess = false;
+  bool _obscureLoginPass = true, _obscureRegisterPass = true, _obscureConfirmPass = true;
+  bool _isLoginProcess = false, _isRegisterProcess = false;
 
   bool get obscureLoginPass => _obscureLoginPass;
   bool get isLoginProcess => _isLoginProcess;
+  bool get obscureRegisterPass => _obscureRegisterPass;
+  bool get obscureConfirmPass => _obscureConfirmPass;
+  bool get isRegisterProcess => _isRegisterProcess;
 
   set obscureLoginPass(bool val) {
     _obscureLoginPass = val;
@@ -14,6 +17,21 @@ class AuthProvider with ChangeNotifier {
 
   set isLoginProcess(bool val) {
     _isLoginProcess = val;
+    notifyListeners();
+  }
+
+  set obscureRegisterPass(bool val) {
+    _obscureRegisterPass = val;
+    notifyListeners();
+  }
+
+  set obscureConfirmPass(bool val) {
+    _obscureConfirmPass = val;
+    notifyListeners();
+  }
+
+  set isRegisterProcess(bool val) {
+    _isRegisterProcess = val;
     notifyListeners();
   }
 }
